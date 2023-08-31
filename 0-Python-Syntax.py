1809d54979d2bce14b59ff6b21ebb24041681310
John Batshon <142701108+JohnBatshon@users.noreply.github.com>
2023-08-31 16:09:55 -0700

    Create 0-Python-Syntax.py

---
+++ 0-Python-Syntax.py
@@ -0,0 +1,28 @@
+def test():
+	print("test")
+	print("test2")
+
+	string = "string"
+	number = 10
+
+	print(string)
+	print(number)
+
+x = 1 + 2 \
+	+ 3
+
+print(x)
+
+y = 1    +    1
+print(y)
+
+
+# Helpful commands when in in the CLI
+# use the following command:
+# python3
+# Once you have exexuted Python3 you can now run Python3 commands within the CLI
+# A great way to get information about certain commands would be to run the following
+# help(print)
+# the above command will pull all available help information about the print function.
+# similarly the following command can also be of some help.
+# dir(print)
