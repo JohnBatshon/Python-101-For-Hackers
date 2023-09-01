a737839436b7573c8f2900ab5e7ef69daf0ad3a8
John Batshon <142701108+JohnBatshon@users.noreply.github.com>
2023-09-01 15:59:27 -0700

    Create 4-Booleans-Operators.py

---
+++ 4-Booleans-Operators.py
@@ -0,0 +1,38 @@
+valid = True
+not_valid = False
+
+print(valid)
+print(type(valid))
+print(not_valid)
+print(type(not_valid))
+
+
+print(valid == True)
+print(not_valid == True)
+
+print(valid != True)
+print(not_valid != True)
+
+print(not valid)
+print(not not_valid)
+
+print("_-_-_-_-_")
+
+print((10 < 9) == True)
+print((10 == 10) == True)
+print((10 != 10) == True)
+print((10 >= 10) == True)
+print((10 <= 10) == True)
+print((10 > 9) == True)
+
+print("_-_-_-_-_")
+
+print((10 < 9))
+print((10 == 10))
+print((10 != 10))
+print((10 >= 10))
+print((10 <= 10))
+print((10 > 9))
+
+print("_-_-_-_-_")
+
