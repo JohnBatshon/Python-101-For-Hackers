1a30936535f40f792a956c09ca5129fc07024929
John Batshon <142701108+JohnBatshon@users.noreply.github.com>
2023-09-01 16:27:39 -0700

    Update 4-Booleans-Operators.py

--- 4-Booleans-Operators.py
+++ 4-Booleans-Operators.py
@@ -36,3 +36,120 @@ print((10 > 9))
 
 print("_-_-_-_-_")
 
+print(10 > 5 and 10 < 5)
+print(10 > 5 or 10 < 5)
+
+print(bool(0))
+print(bool(1))
+
+
+print("_-_-_-_-_")
+
+print(bool(0) == False)
+print(bool(1) == True)
+
+print("_-_-_-_-_")
+print("10 + 10")
+print(10 + 10)
+
+print("_-_-_-_-_")
+
+print("10 - 10")
+print(10 - 10)
+
+print("_-_-_-_-_")
+
+print("10 / 10")
+print(10 / 10)
+
+print("_-_-_-_-_")
+
+print("10 // 10")
+print(10 // 10)
+
+print("_-_-_-_-_")
+
+print(10 / 3)
+print(10 // 3)
+print(10 % 3)
+
+print("_-_-_-_-_")
+
+print("10 * 10")
+print(10 * 10)
+
+print("_-_-_-_-_")
+
+print("10 ** 10")
+print(10 ** 10)
+
+print("_-_-_-_-_")
+
+print("10 % 10")
+print(10 % 10)
+
+print("_-_-_-_-_")
+
+x = 10
+print(x)
+
+x = x + 1
+print(x)
+
+x += 1
+print(x)
+
+x -= 1
+print(x)
+
+x *= 1
+print(x)
+
+x /= 1
+print(x)
+
+print("_-_-_-_-_")
+
+x *= 5
+print(x)
+
+x /= 5
+print(x)
+
+print("_-_-_-_-_")
+
+x = 13
+print(bin(x))
+print(bin(x)[2:].rjust(4,"0"))
+
+print("_-_-_-_-_")
+
+y = 5
+print(y)
+print(bin(y))
+print(bin(y)[2:].rjust(4,"0"))
+
+print("_-_-_-_-_")
+
+# Bitwise AND
+print(bin(x & y)[2:].rjust(4,"0"))
+print(x & y)
+
+# Bitwise OR
+print(bin(x | y)[2:].rjust(4,"0"))
+
+print("_-_-_-_-_")
+
+print(bin(x >> 1)[2:].rjust(4,"0"))
+print(bin(x >> 2)[2:].rjust(4,"0"))
+print(bin(x >> 3)[2:].rjust(4,"0"))
+print(bin(x >> 4)[2:].rjust(4,"0"))
+
+print("_-_-_-_-_")
+
+print(bin(x << 1)[2:].rjust(4,"0"))
+print(bin(x << 2)[2:].rjust(4,"0"))
+print(bin(x << 3)[2:].rjust(4,"0"))
+print(bin(x << 4)[2:].rjust(4,"0"))
+
+print("_-_-_-_-_")
