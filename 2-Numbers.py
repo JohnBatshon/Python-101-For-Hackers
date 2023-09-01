113df61671dedeeb6cd9bf5bd0081e79c8a9d973
John Batshon <142701108+JohnBatshon@users.noreply.github.com>
2023-09-01 12:53:36 -0700

    Create 2-Numbers.py

---
+++ 2-Numbers.py
@@ -0,0 +1,47 @@
+
+# Example Integer
+t1_int = 1
+print(t1_int)
+# Verifies the Type
+print(type(t1_int))
+
+# Example Float
+t1_float = 1.0
+print(t1_float)
+# Verifies the Type
+print(type(t1_float))
+
+# Example Complex Number
+t1_complex = 3.14j
+print(t1_complex)
+# Verifying the Type
+print(type(t1_complex))
+
+# Example of Hexidecimal of the Number 10
+t1_hex = 0xa
+print(t1_hex)
+print(type(t1_hex))
+
+# Example of an Octal Number 8
+t1_octal = 0o10
+print(t1_octal)
+print(type(t1_octal))
+
+# Eample of adding different number types (Integer + Hexicimal + Octal)
+print(1 + 0x1 + 0o1)
+
+# Examples of Absolute Numbers
+print(abs(4))
+print(abs(-4))
+
+# Examples of Rounding Numbers
+print(round(8.4))
+print(round(8.5))
+print(round(8.6))
+print(round(8.7))
+
+# Example of a Binary Number
+print(bin(8))
+
+# Example of a Hex Number
+print(hex(8))
