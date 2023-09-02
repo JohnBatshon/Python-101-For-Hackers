6d505f6e0bf018fb9df6f54c7622042cc1336a2d
John Batshon <142701108+JohnBatshon@users.noreply.github.com>
2023-09-02 14:20:17 -0700

    Create 10-Loops.py

---
+++ 10-Loops.py
@@ -0,0 +1,28 @@
+# ##################################################################################################################################
+# Loops
+# ##################################################################################################################################
+#
+# In Python 3, "loops" are programming constructs that allow you to repeat a set of instructions multiple times,
+# making your code more efficient and less repetitive.
+# They are like a recipe that you follow repeatedly until a certain condition is met.
+#
+# There are two primary types of loops in Python:
+#
+# for loop: A for loop is used when you know in advance how many times you want to repeat a block of code.
+# It iterates over a sequence (like a list or range) and performs the specified actions for each item in the sequence.
+#
+# for item in sequence:
+#     Code to execute for each item in the sequence
+#
+# while loop: A while loop is used when you want to repeat a block of code as long as a certain condition is true.
+# It keeps looping until the condition becomes false.
+#
+# while condition:
+#     Code to execute while the condition is true
+#
+# Loops are incredibly useful for tasks such as processing data, performing repetitive calculations,
+# or iterating through collections of items.
+# They help you automate tasks and make your code more flexible and powerful.
+#
+# ##################################################################################################################################
+
