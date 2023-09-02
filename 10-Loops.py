b30986b1a4fbbe1deda585e62d31732ed9aec6fe
John Batshon <142701108+JohnBatshon@users.noreply.github.com>
2023-09-02 14:57:16 -0700

    Update 10-Loops.py

--- 10-Loops.py
+++ 10-Loops.py
@@ -26,3 +26,111 @@
 #
 # ##################################################################################################################################
 
+# Although you can use a command like the below example it is not best practice and leaves room for human error.
+# Result:
+# 1
+# 2
+# 3
+
+a = 1
+print(a)
+a += 1
+print(a)
+a += 1
+print(a)
+
+print("---")
+
+# Using a while loop in the example below will keep running until a no longer less than 5
+# Results:
+# 1
+# 2
+# 3
+# 4
+# 5
+
+a = 1
+while a < 5:
+    a += 1
+    print(a)
+
+print("---")
+
+# In the for loop we specify multiple times triggering it to run the command i+6 against each variable.
+# Results:
+# 6
+# 7
+# 8
+# 9
+# 10
+
+for i in [0, 1, 2, 3, 4]:
+    print(i+6)
+
+print("---")
+
+# Using the range command we are limiting to no more than 5 entires.
+# Resulting: 6 7 8 9 10
+
+for i in range(5):
+    print(i+6)
+
+print("---")
+
+#
+
+for i in range(3):
+    for j in range (3):
+        print(i,j)
+
+print("---")
+
+# In this example i will increase by 1 until it hits 2 then the loop will break.
+# Result 0 1
+
+for i in range(5):
+    if i == 2:
+        break
+    print(i)
+
+print("---")
+
+# In this example using the continue parameter will continue the process without hitting the print(i) command when i = 2
+# Resulting: 0 1 3 4
+
+for i in range(5):
+    if i == 2:
+        continue
+    print(i)
+
+print("---")
+
+# When using the pass control statement when i == 2 it will trigger the pass command and move onto the next action print(i)
+# Resulting: 0 1 2 3 4
+
+for i in range(5):
+    if i == 2:
+        pass
+    print(i)
+
+print("---")
+
+# So in this example we use a string in place of a number. The command will cycle through the word "string" and each time it will
+# pass 1 letter through and then print it in the 2nd lind of code.
+# Resulting: s t r i n g
+
+for c in "string":
+    print(c)
+
+print("---")
+
+# In this example we use a key + value set and run the command over each of the 3 indexed items.
+# Results:
+# a 1
+# b 2
+# c 3
+
+for key, value in {"a":1, "b":2, "c":3}.items():
+    print(key, value)
+
+print("---")
