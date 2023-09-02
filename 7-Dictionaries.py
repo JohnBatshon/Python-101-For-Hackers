7bc9b7b72aa6844cab81ca1829e24f49d6bb7052
John Batshon <142701108+JohnBatshon@users.noreply.github.com>
2023-09-02 09:05:57 -0700

    Update 7-Dictionaries.py

--- 7-Dictionaries.py
+++ 7-Dictionaries.py
@@ -43,14 +43,63 @@ print(dict1.values())
 print(dict1.items())
 
 # The below command will not work because "a" has already been assigned.
+# Results (No Change): {'a': 1, 'b': 2, 'c': 3}
 
 print(dict1)
 dict1["a"] = 1
 print(dict1)
 
-# 
+# Since "d" was not assigned I was able to add in a value for "d" of 4
+# Results: {'a': 1, 'b': 2, 'c': 3, 'd': 4}
 
 print(dict1)
 dict1["d"] = 4
 print(dict1)
 
+# Changing the Value of Key "A". Changed from 1 to 0
+# Results: {'a': 0, 'b': 2, 'c': 3, 'd': 4}
+print(dict1)
+dict1["a"] = 0
+print(dict1)
+
+# Using the .update command we can change the key value of "a" back to 1
+# Results: {'a': 1, 'b': 2, 'c': 3, 'd': 4}
+
+print(dict1)
+dict1.update({"a": 1})
+print(dict1)
+
+# Using the .pop command to remove a key value
+# Results: {'a': 1, 'b': 2, 'c': 3}
+
+print(dict1)
+dict1.pop("d")
+print(dict1)
+
+# Using the del command to remove a key value
+# Results: {'a': 1, 'b': 2}
+
+print(dict1)
+del dict1["c"]
+print(dict1)
+
+# Adding a value in for key "c"
+# Results:
+# {'a': 1, 'b': 2}
+# {'a': 1, 'b': 2, 'c': {'a': 1, 'b': 2}}
+
+print(dict1)
+dict1["c"] = {"a":1, "b":2}
+print(dict1)
+
+# Empty Dictionary Example 1
+# Results: []
+
+dict2 = []
+print(dict2)
+
+# Empty Dictionary Example 2
+# Results: ()
+
+dict3 = ()
+print(dict3)
