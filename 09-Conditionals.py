7cecc059a506f4629c37679cd1da696856969a87
John Batshon <142701108+JohnBatshon@users.noreply.github.com>
2023-09-02 13:24:35 -0700

    Create 09-Conditionals.py

---
+++ 09-Conditionals.py
@@ -0,0 +1,35 @@
+# ########################################################################################################################
+# Conditionals
+# ########################################################################################################################
+# 
+# In Python 3, "conditionals" refer to programming constructs that allow you to make decisions in your code based on certain conditions.
+# They help your program execute different sets of instructions depending on whether a specified condition is true or false.
+# 
+# The primary conditional statements in Python are:
+#
+# If:  It is used to execute a block of code only if a specified condition is true.
+# if condition:
+#     Code to execute if condition is true
+#
+# else: It is used with if to execute a different block of code when the condition is false.
+#
+# if condition:
+#      Code to execute if condition is true
+# else:
+#      Code to execute if condition is false
+#
+# elif (else if): It is used to check multiple conditions sequentially and execute the first block of code where the condition is true.
+#
+# if condition1:
+#      Code to execute if condition1 is true
+# elif condition2:
+#     Code to execute if condition2 is true
+# else:
+#     Code to execute if neither condition1 nor condition2 is true
+#
+# Conditionals are fundamental for controlling the flow of your Python programs and making them responsive to different situations.
+# They allow you to create dynamic and intelligent applications by directing the program's execution path based on the data and 
+# conditions encountered during runtime.
+# ########################################################################################################################
+
+
