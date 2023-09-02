21482185044432ee2af124ed71193545a87189cd
John Batshon <142701108+JohnBatshon@users.noreply.github.com>
2023-09-02 08:18:39 -0700

    Update 7-Dictionaries.py

---
+++ 7-Dictionaries.py
@@ -0,0 +1,18 @@
+# ####################################################################################################################################
+# Dictionaries
+# ####################################################################################################################################
+# A data structure that stores key-value pairs.
+# Unordered and mutable, meaning you can change its content.
+# Created using curly braces {} or the dict() constructor.
+# Accessed and manipulated using keys.
+# Keys are unique and must be immutable (e.g., strings, numbers).
+# Values can be of any data type (e.g., numbers, strings, lists).
+#
+# Example
+#
+# my_dict = {"name": "John", "age": 30}
+# print(my_dict["name"])  # Output: "John"
+#
+# Dictionaries are commonly used for mapping relationships between data.
+# ####################################################################################################################################
+
