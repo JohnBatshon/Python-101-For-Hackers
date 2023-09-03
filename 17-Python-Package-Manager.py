ee7811e4a5f476d325c07d4d919b4239dae96079
John Batshon <142701108+JohnBatshon@users.noreply.github.com>
2023-09-03 07:51:17 -0700

    Create 17-Python-Package-Manager.py

---
+++ 17-Python-Package-Manager.py
@@ -0,0 +1,45 @@
+# ########################################################################################################################
+#
+# Python Package Manager
+#
+# ########################################################################################################################
+#
+# Pthyon Packages can be viewed at https://pypi.org
+# Find, install, and publish Python Packages with the Python Package Index.
+#
+# Possible Pre-Reqs
+#
+# apt-get install python-dev
+#
+# Example Pwntools
+#
+# pip list
+# 
+# pip install pwntools
+#
+# pip freeze
+#
+# pip freeze | less
+#
+# Can install exact versions with a command like the following
+# 
+# pip install pwntools==4.10.0
+#
+# To uninstall
+#
+# pip uninstall pwntools
+#
+# python3
+# >>> from pwn import *
+# >>> exit()
+#
+#
+#
+#
+#
+#
+#
+# ########################################################################################################################
+
+set_random = 1
+
