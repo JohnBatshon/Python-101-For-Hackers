f39bb8d04d454e21b45691f71783b9045c4b034c
John Batshon <142701108+JohnBatshon@users.noreply.github.com>
2023-09-02 20:35:51 -0700

    Create 15-Functions-and-Code-Reuse.py

---
+++ 15-Functions-and-Code-Reuse.py
@@ -0,0 +1,18 @@
+# ##############################################################################################################################
+#
+# Dunctions and Code Reuse
+#
+# ##############################################################################################################################
+#
+# Functions in Python allow you to:
+#
+# Encapsulate Code: Group code into reusable blocks.
+# Modularize: Divide your program into manageable parts.
+# Reuse: Call the same code multiple times.
+# Parameters: Pass data into functions.
+# Return Values: Get results from functions.
+# Abstraction: Hide implementation details.
+# Enhance Readability: Make code more organized and understandable.
+#
+# ##############################################################################################################################
+
