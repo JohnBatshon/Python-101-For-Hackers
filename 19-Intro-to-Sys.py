5e065fc116b77227405b6dd9dbd0048325bef05d
John Batshon <142701108+JohnBatshon@users.noreply.github.com>
2023-09-03 08:32:08 -0700

    Update 19-Intro-to-Sys.py

--- 19-Intro-to-Sys.py
+++ 19-Intro-to-Sys.py
@@ -29,6 +29,14 @@ print(sys.argv)
 
 if len(sys.argv) !=3:
 	print("[X] To run {} enter a username and password".format(sys.argv[0]))
+	sys.exit(5)
 
 username = sys.argv[1]
 password = sys.argv[2]
+
+print("{} {}".format(username,password))
+
+print(sys.path)
+print(sys.modules)
+
+sys.exit(0)
